a1ab9f0d1e4def07eee9ff96d7cb6861bf82bd43
siit2020 <soporte_tecnico@si1t.com>
2020-12-15 09:38:21 -0600

    Buttons1

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -2,7 +2,7 @@
 @section('vistas')
     <h1>Mostrando las noticias y publicidades</h1>
   <a class="btn btn-primary" href="{{url('/noticias')}}">Ver Noticias</a>
-  <a class="btn btn-primary" href="{{url('/noticias')}}">Ver Publicidad</a>
+  <a class="btn btn-primary" href="{{url('/noticias')}}">Ver Publicidades</a>
 @endsection
 @section('tabla')
     @include('Panel.Noticias.index')
